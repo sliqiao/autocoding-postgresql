a20b6ccd5a18f6a17ed15ce592fe067a24a6df76
liqiao <liqiao@tumyu.com>
2020-07-15 16:24:01 +0800

    针对天有科技代码进行适配，代码生成工具

--- src/main/java/com/autocoding/codebuilder/JpaRepositoryBuilder.java
+++ src/main/java/com/autocoding/codebuilder/JpaRepositoryBuilder.java
@@ -9,17 +9,18 @@ import com.autocoding.model.Project;
 
 /**
  * 
- * @ClassName: GwtModelBuilder
+ * @ClassName: MybatisMapperBuilder
  * @Description:TODO(这里用一句话描述这个类的作用)
  * @author: QiaoLi
- * @date: Jul 15, 2020 2:13:27 PM
+ * @date: Jul 15, 2020 2:14:34 PM
  */
-@CodeBuilderAnnotation(desc = "生成GWT Model")
-public class GwtModelBuilder extends BaseCodeBuilder {
+@CodeBuilderAnnotation(desc = "生成Mybatis Mapper配置文件")
+public class JpaRepositoryBuilder extends BaseCodeBuilder {
 
-	public GwtModelBuilder(Project project) {
+	public JpaRepositoryBuilder(Project project) {
 		super(project);
-		super.fileoutputPath = super.project.getEntitiesPath() + super.entity.getEntityName() + "Model.java";
+		this.fileoutputPath = this.project.getServicePath() + this.entity.getEntityName() + "Repo.java";
+
 	}
 
 	@Override
@@ -29,4 +30,5 @@ public class GwtModelBuilder extends BaseCodeBuilder {
 				CodeBuilderContainer.getTempleateFileName(this.getClass()), super.fileoutputPath);
 
 	}
+
 }
