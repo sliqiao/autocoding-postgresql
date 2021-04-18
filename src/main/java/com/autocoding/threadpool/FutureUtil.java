b19e6fe4e1868f376980bdd1b3f905d8a168c70a
QiaoLi <243368584@qq.com>
2021-04-18 21:25:37 +0800

    1:线程池增加建造者设计模式 2：Future实现非阻塞模式下的回调模式

---
+++ src/main/java/com/autocoding/threadpool/FutureUtil.java
@@ -0,0 +1,37 @@
+package com.autocoding.threadpool;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public final class FutureUtil {
+	private static final ExecutorService EXECUTOR_SERVICE = Executors.newSingleThreadExecutor();
+
+	private FutureUtil() {
+	}
+
+	public static <T> void get(Future<T> future, Callback<T> callback) {
+
+		FutureUtil.EXECUTOR_SERVICE.execute(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					final T result = future.get();
+					callback.sucess(result);
+				} catch (final InterruptedException e) {
+					callback.onError(e);
+				} catch (final ExecutionException e) {
+					callback.onError(e);
+				}
+			}
+		});
+
+	}
+
+	public static interface Callback<T> {
+		T sucess(T result);
+		void onError(Throwable throwable);
+	}
+}
